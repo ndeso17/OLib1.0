010e17edfa2c3498fc7d77733619c283bb7684b2
xtkjone23 <50276466+ndeso17@users.noreply.github.com>
2021-02-07 14:56:44 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION['isLogin'])){
-        header("Location: login.php");
-    }else {
-        echo 'Mungkin Masih Tersimpan Sesi Terakhir <a href="logout.php">LoogOut</a>';        
-    }
-?>
